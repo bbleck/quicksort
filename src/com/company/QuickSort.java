7a80185c84dec33abeab26742e68a1784ca5a459
Brian Bleck <bbleck@unm.edu>
2019-11-21 00:16:37 -0700

    Fixed some comment wording for clarity.

--- src/com/company/QuickSort.java
+++ src/com/company/QuickSort.java
@@ -45,21 +45,22 @@ public class QuickSort {
         int border = low + 1;//border is now the index of the first non-pivot value
         //iterate through the array from border index to high index
         for (int i = border; i <= high; i++) {
-            //our low index now holds the pivot, if the value at index i is less than our pivot value,
-            // swap the value at i with the value at the current border index and then increment the border index by one
-            // the border is simply the first index where the values bigger than the pivot value begin
-            // the values left of the border will all be smaller than the pivot and the values at the border and to its
-            // right will all be larger than the border
-            // tldr: we iterate through the array and push smaller values than our pivot onto the left of the array,
-            // keeping track of the border between smaller and bigger
+            // Our low index now holds the pivot.
+            // If the value at index i is less than our pivot value,
+            // swap the value at i with the value at the current border index and then increment the border index by one.
+            // The border is simply the first index where the values bigger than the pivot value begin.
+            // The values left of the border will all be smaller than the pivot and the values at the border and to its
+            // right will all be larger than the border.
+            // tldr: We iterate through the array and push smaller values than our pivot onto the left of the array,
+            // keeping track of the border between smaller and bigger.
             if (inputArr[i] < inputArr[low]) {
                 swap(inputArr, i, border++);
             }
         }
-        // now that we have partitioned the smaller from the larger values,
-        // we swap the pivot value with border-1, which is the index of the last smaller value
-        // then we return border-1 as the pivot index value, because that's where pivot is now
+        // Now that we have partitioned the smaller from the larger values,
+        // we swap the pivot value with border-1, which is the index of the last smaller value.
         swap(inputArr, low, border-1);
+        // Then we return border-1 as the pivot index value, because that's where pivot is now located in the array.
         return border-1;
     }
 }
